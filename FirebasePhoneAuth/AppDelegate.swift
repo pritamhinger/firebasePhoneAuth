51e91f7fd0a840ce10a52bd04f01d237bf8154f4
pritam hinger <pritamgenius24@gmail.com>
2017-07-24 00:19:31 +0530

    Configure Remote Notifications. Add Register, Login and Logout functions.

--- FirebasePhoneAuth/AppDelegate.swift
+++ FirebasePhoneAuth/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,10 +16,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if Auth.auth().canHandleNotification(userInfo){
+            completionHandler(UIBackgroundFetchResult.noData)
+            return
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Pass device token to auth
+        print("Device Token is \(deviceToken)")
+        Auth.auth().setAPNSToken(deviceToken, type: AuthAPNSTokenType.prod)
+        
+        // Further handling of the device token if needed by the app
+        // ...
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- FirebasePhoneAuth/ViewController.swift
+++ FirebasePhoneAuth/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var verificationCodeTextField: UITextField!
+    @IBOutlet weak var phoneNumberField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        logoutButton.isHidden = true
+        if let uid = Auth.auth().currentUser?.uid{
+            print("Your user id is \(uid)")
+            logoutButton.isHidden = false
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func logout(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()
+        }
+        catch let error{
+            print(error)
+        }
+    }
+    
+    @IBAction func authenticate(_ sender: Any) {
+        print("Initiating authentication")
+        PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumberField.text!, completion: { (verificationId, error) in
+            if error != nil{
+                print(error!)
+                return
+            }
+            
+            UserDefaults.standard.set(verificationId, forKey: "authVerificationID")
+            print("Authentication called")
+        })
     }
 
-
+    @IBAction func verifyCode(_ sender: Any) {
+        if let verificationID = UserDefaults.standard.string(forKey: "authVerificationID"){
+            let credential = PhoneAuthProvider.provider().credential(withVerificationID: verificationID, verificationCode: verificationCodeTextField.text!);
+            Auth.auth().signIn(with: credential, completion: { (user, error) in
+                if error != nil{
+                    print(error!)
+                    return
+                }
+                
+                print(user!)
+            })
+        }
+    }
 }
 
